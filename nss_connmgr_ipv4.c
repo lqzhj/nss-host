1c9b225b7538f2a409c754ebd2be47e7778a6f31
Radhakrishna Jiguru <rjiguru@codeaurora.org>
2013-08-27 23:57:48 +0530

    [ipq 806x] qca-nss-drv : CR Fixed:534889
    Added the License information to files
    
    Change-Id: Id3893454629ff49b6ecdfddc52f899a16386a661
    Signed-off-by: Radhakrishna Jiguru <rjiguru@codeaurora.org>
    Signed-off-by: Murat Sezgin <msezgin@codeaurora.org>
    Reviewed-by: Bob Amstadt <ramstadt@codeaurora.org>

--- nss_connmgr_ipv4.c
+++ nss_connmgr_ipv4.c
@@ -1,4 +1,18 @@
-/* * Copyright (c) 2013 Qualcomm Atheros, Inc. * */
+/*
+ **************************************************************************
+ * Copyright (c) 2013, Qualcomm Atheros, Inc.
+ * Permission to use, copy, modify, and/or distribute this software for
+ * any purpose with or without fee is hereby granted, provided that the
+ * above copyright notice and this permission notice appear in all copies.
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+ * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ **************************************************************************
+ */
 
 /*
  * nss_connmgr_ipv4.c
@@ -8,7 +22,6 @@
  * connection track module for updating/syncing connection level information
  * between the two.It is responsible for maintaing  all connection (flow) level
  * information and statistics for all fast path connections.
- *
  * ------------------------REVISION HISTORY-----------------------------
  * Qualcomm Atheros         01/Mar/2013              Created
  */
