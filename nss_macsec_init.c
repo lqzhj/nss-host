33fb7629521d323a1bd09a9da5413ac84bc3e25e
slv <slv@codeaurora.org>
2015-09-09 15:49:54 +0800

    [mac-sec]:to support macsec in kernel 3.14
    Change-Id: I99a57d76332f8e4de812a3a8048b43b3f2a3fdab
    Signed-off-by: slv <slv@codeaurora.org>

--- nss_macsec_init.c
+++ nss_macsec_init.c
@@ -18,7 +18,13 @@
 #include <linux/kernel.h>
 #include <net/sock.h>
 #include <asm/io.h>
+#include <linux/version.h>
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(3, 14, 0))
 #include <mach/msm_iomap.h>
+#else
+#include <linux/of.h>
+#endif
+
 #include <linux/platform_device.h>
 #include <linux/resource.h>
 #include <linux/types.h>
@@ -131,6 +137,11 @@ static void nss_macsec_netlink_recv(struct sk_buff *__skb)
 	struct sdk_msg_header *header = NULL;
 	void *msg_data;
 	u32 pid = 0, msg_type = 0, sync = 0, ret = 0, msg_len = 0;
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0))
+#define		MAX_PAYLOAD 2048
+	struct nlmsghdr *nlh2 = NULL;
+	struct sk_buff *skb2 = NULL;
+#endif
 
 	if ((skb = skb_get(__skb)) != NULL) {
 		nlh = nlmsg_hdr(skb);
@@ -150,10 +161,21 @@ static void nss_macsec_netlink_recv(struct sk_buff *__skb)
 			printk("Unexpected msg:0x%x received!\n", msg_type);
 		}
 
-		NETLINK_CB(skb).pid = 0;	/* from kernel */
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0))
+		skb2 = nlmsg_new(MAX_PAYLOAD, GFP_KERNEL);
+		if (skb2 == NULL) {
+			printk(KERN_ERR "Failed to alloc skb\n");
+			return;
+		}
+
+		nlh2 = nlmsg_put(skb2, 0, 0, 0, MAX_PAYLOAD, 0);
+		memcpy(NLMSG_DATA(nlh2), msg_data, msg_len);
+		netlink_unicast(sdk_nl_sk, skb2, pid, 0);
+#else
+		NETLINK_CB(skb).pid = 0;		/* from kernel */
 		NETLINK_CB(skb).dst_group = 0;	/* unicast */
 		netlink_unicast(sdk_nl_sk, skb, pid, MSG_DONTWAIT);
-
+#endif
 	}
 
 	return;
@@ -161,11 +183,19 @@ static void nss_macsec_netlink_recv(struct sk_buff *__skb)
 
 static int nss_macsec_netlink_init(void)
 {
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0))
+	struct netlink_kernel_cfg cfg = {
+			.input			= nss_macsec_netlink_recv,
+	};
+
+	sdk_nl_sk = netlink_kernel_create(&init_net, NETLINK_SDK, &cfg);
+#else
 	sdk_nl_sk = netlink_kernel_create(&init_net,
-					  NETLINK_SDK,
-					  0,
-					  nss_macsec_netlink_recv,
-					  NULL, THIS_MODULE);
+						NETLINK_SDK,
+						0,
+						nss_macsec_netlink_recv,
+						NULL, THIS_MODULE);
+#endif
 
 	if (sdk_nl_sk == NULL) {
 		printk("Create netlink socket fail\n");
@@ -183,13 +213,77 @@ static void nss_macsec_netlink_fini(void)
 	}
 }
 
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0))
+static int nss_macsec_dt_init(uint32_t dev_id)
+{
+	int ret = 0;
+	unsigned int mem_start, mem_len;
+	void __iomem *mmap_io_addr = NULL;
+	struct device_node *nss_macsec_node = NULL;
+	const __be32 *reg_cfg;
+	unsigned int len = 0;
+	char dev_name[32];
+
+	if (nss_macsec_pre_init_flag == 0) {
+		nss_macsec_pre_init();
+		nss_macsec_pre_init_flag = 1;
+	}
+
+	memset(dev_name, 0, sizeof(dev_name));
+	snprintf(dev_name, sizeof(dev_name), "nss-macsec%d", dev_id);
+
+	nss_macsec_node = of_find_node_by_name(NULL, dev_name);
+	if (!nss_macsec_node) {
+		printk(KERN_ERR "cannot find nss-macsec%d node\n", dev_id);
+		return -EIO;
+	}
+	printk(KERN_INFO "%s DT exist!\n", dev_name);
+
+	reg_cfg = of_get_property(nss_macsec_node, "reg", &len);
+	if (!reg_cfg) {
+		printk(KERN_ERR "%s: error reading device node properties for reg\n", dev_name);
+		return -EIO;
+	}
+
+	mem_start = be32_to_cpup(reg_cfg);
+	mem_len = be32_to_cpup(reg_cfg + 1);
+
+	if (!request_mem_region(mem_start, mem_len, dev_name)) {
+		macsec_warning("%s Unable to request resource.", __func__);
+		return -EIO;
+	}
+
+	mmap_io_addr = ioremap_nocache(mem_start, mem_len);
+	if (!mmap_io_addr) {
+		macsec_warning("%s ioremap fail.", __func__);
+		return -EIO;
+	}
+	macsec_trace("macsec.%d phy_addr:0x%x len:0x%x mem_start:%p\n",
+		     dev_id, mem_start, mem_len, mmap_io_addr);
+
+	macsec_ctx.macsec_base[dev_id] = mmap_io_addr;
+
+	/* Invoke Macsec Initialization API */
+	nss_macsec_secy_init(dev_id);
+
+	if (macsec_notifier_register_status == 0) {
+		nss_gmac_link_state_change_notify_register(&macsec_notifier);
+		macsec_notifier_register_status = 1;
+	}
+
+	macsec_trace("macsec.%d probe done\n", dev_id);
+	return ret;
+
+}
+#endif
+
 static int nss_macsec_probe(struct platform_device *pdev)
 {
 	int ret = 0;
 	unsigned int mem_start, mem_len;
 	void __iomem *mmap_io_addr = NULL;
 
-	if(nss_macsec_pre_init_flag == 0) {
+	if (nss_macsec_pre_init_flag == 0) {
 		nss_macsec_pre_init();
 		nss_macsec_pre_init_flag = 1;
 	}
@@ -214,7 +308,7 @@ static int nss_macsec_probe(struct platform_device *pdev)
 	/* Invoke Macsec Initialization API */
 	nss_macsec_secy_init(pdev->id);
 
-	if(macsec_notifier_register_status == 0) {
+	if (macsec_notifier_register_status == 0) {
 		nss_gmac_link_state_change_notify_register(&macsec_notifier);
 		macsec_notifier_register_status = 1;
 	}
@@ -231,7 +325,7 @@ static int nss_macsec_remove(struct platform_device *pdev)
 	mem_start = pdev->resource[0].start;
 	mem_len = pdev->resource[0].end - mem_start + 1;
 
-	if(macsec_notifier_register_status) {
+	if (macsec_notifier_register_status) {
 		nss_gmac_link_state_change_notify_unregister(&macsec_notifier);
 		macsec_notifier_register_status = 0;
 	}
@@ -257,11 +351,18 @@ static int __init nss_macsec_init_module(void)
 	uint32_t dev_id = 0;
 	for(dev_id=0; dev_id<MACSEC_DEVICE_NUM; dev_id++)
 		macsec_ctx.macsec_base[dev_id] = NULL;
+
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0))
+	for (dev_id = 0; dev_id < MACSEC_DEVICE_NUM; dev_id++) {
+		if (nss_macsec_dt_init(dev_id) != 0)
+			return -EIO;
+	}
+#else
 	if (platform_driver_register(&nss_macsec_drv) != 0) {
 		macsec_warning("platform drv reg failure\n");
 		return -EIO;
 	}
-
+#endif
 	macsec_trace("%s done!\n", __func__);
 
 	return 0;
@@ -285,8 +386,10 @@ static void __exit nss_macsec_fini(void)
 	nss_macsec_netlink_fini();
 
 	nss_macsec_mutex_destroy();
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(3, 14, 0))
 	platform_driver_unregister(&nss_macsec_drv);
-	if(nss_macsec_pre_init_flag) {
+#endif
+	if (nss_macsec_pre_init_flag) {
 		nss_macsec_pre_exit();
 		nss_macsec_pre_init_flag = 0;
 	}
