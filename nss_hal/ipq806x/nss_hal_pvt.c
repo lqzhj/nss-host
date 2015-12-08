67c8675cc489faa37f2e6b4318d61eacc919091b
Shyam Sunder <ssunde@codeaurora.org>
2015-12-08 13:00:56 +0530

    [qca-nss-drv] Fixed the wrong PPTP enable flag for coconut
    Change-Id: I86c5d042065f313322b4f88c63287eb71416761e
    Signed-off-by: Shyam Sunder <ssunde@codeaurora.org>

--- nss_hal/ipq806x/nss_hal_pvt.c
+++ nss_hal/ipq806x/nss_hal_pvt.c
@@ -887,7 +887,7 @@ static struct nss_platform_data *nss_hal_of_get_pdata(struct device_node *np,
 	    || of_property_read_u32(np, "qcom,tun6rd_enabled", &npd->tun6rd_enabled)
 	    || of_property_read_u32(np, "qcom,l2tpv2_enabled", &npd->l2tpv2_enabled)
 	    || of_property_read_u32(np, "qcom,tunipip6_enabled", &npd->tunipip6_enabled)
-	    || of_property_read_u32(np, "qcom,pptp_enabled", &npd->tunipip6_enabled)
+	    || of_property_read_u32(np, "qcom,pptp_enabled", &npd->pptp_enabled)
 	    || of_property_read_u32(np, "qcom,shaping_enabled", &npd->shaping_enabled)
 	    || of_property_read_u32(np, "qcom,wlan_dataplane_offload_enabled", &npd->wifioffload_enabled)
 	    || of_property_read_u32(np, "qcom,portid_enabled", &npd->portid_enabled)) {
