bb143ea6c92f07c3dea0d01f4faaf76a5c4c9037
Bhaskar Valaboju <bvalaboj@codeaurora.org>
2017-02-20 10:26:13 +0530

    [qca-nss-clients] nss-lag client support for 3.14 and 4.4 kernel
    Change-Id: I48de37becc8b83a752b4e33c997a79293284ae30
    Signed-off-by: Bhaskar Valaboju <bvalaboj@codeaurora.org>

--- lag/nss_lag.c
+++ lag/nss_lag.c
@@ -56,7 +56,9 @@
 #endif
 #endif /* CONFIG_DYNAMIC_DEBUG */
 
-#define NSS_LAG_MAX_BOND_DEVICES 2
+#define NSS_LAG_MAX_BOND_DEVICES 4
+#define NSS_LAG_MAX_PPE_BOND_DEVICES 2
+#define NSS_LAG_MAX_SLAVES_PER_BONDID 16
 #define NSS_LAG_BOND_ID_INVALID	0xFFFFFFFF
 #define NSS_LAG_BOND_DEV_STATE_ACTIVE 1
 #define NSS_LAG_BOND_DEV_STATE_INACTIVE 2
@@ -66,9 +68,12 @@
  */
 struct nss_bond_pvt {
 	int32_t bond_dev_state;	/* Active or Inactive */
+	int32_t bondid;		/* Bond ID */
 	uint32_t members;	/* Enslaved devices:
-					   bit 0 - eth0,
-					   bit1 - eth1 ... */
+				 *   bit 0 - eth0,
+				 *   bit1 - eth1 ...
+				 */
+	struct net_device *slaves[NSS_LAG_MAX_SLAVES_PER_BONDID];
 } bond_pvt[NSS_LAG_MAX_BOND_DEVICES];
 
 DEFINE_SPINLOCK(nss_lag_spinlock);
@@ -90,7 +95,7 @@ static int nss_lag_update_ppe(int32_t bondid, int32_t slave_ifnum,
 	struct nss_bond_pvt *pvt;
 	int32_t enable = 1;
 
-	if (bondid > NSS_LAG_MAX_BOND_DEVICES) {
+	if (bondid > NSS_LAG_MAX_PPE_BOND_DEVICES) {
 		nss_lag_warn("bondid is wrong: bondid=%d\n", bondid);
 		return -1;
 	}
@@ -158,29 +163,18 @@ static void nss_lag_state_callback(void *ctx, struct nss_lag_msg *nm)
  *	state in the bonding driver.
  */
 static nss_tx_status_t nss_lag_send_lag_state(struct nss_ctx_instance *nss_ctx,
-		struct net_device *master, struct net_device *slave,
+		int32_t bondid, struct net_device *slave,
 		enum nss_lag_state_change_ev slave_state)
 {
 	int32_t lagid = 0;
-	int32_t bondid = 0;
 	int32_t slave_ifnum;
 	nss_tx_status_t nss_tx_status;
 	struct nss_lag_msg nm;
 	struct nss_lag_state_change *nlsc = NULL;
-	int ret;
 
 	nss_lag_info("Send LAG update for: %p (%s)\n", slave, slave->name);
 
 	memset(&nm, 0, sizeof(nm));
-
-	/*
-	 * Figure out the aggregation id of this slave
-	 */
-	bondid = bond_get_id(master);
-	if (bondid == NSS_LAG_BOND_ID_INVALID) {
-		nss_lag_warn("Invalid LAG group id 0x%x\n", bondid);
-		return NSS_TX_FAILURE_BAD_PARAM;
-	}
 
 	lagid = bondid + NSS_LAG0_INTERFACE_NUM;
 
@@ -238,14 +232,10 @@ static void nss_lag_event_cb(void *if_ctx, struct nss_lag_msg *msg)
 static int nss_lag_update_slave(struct netdev_notifier_info *info)
 {
 	struct net_device *slave_dev = netdev_notifier_info_to_dev(info);
-	struct netdev_notifier_changeupper_info *cu_info = info;
-	uint32_t slave_ifnum;
-
-	/*
-	 * Check if the master pointer is valid
-	 */
-	if (!cu_info->master)
-		return NOTIFY_DONE;
+	struct net_device *bond_dev;
+	int32_t bondid = -1;
+	uint32_t slave_ifnum, i;
+	struct nss_bond_pvt *pvt;
 
 	if (!netif_is_bond_slave(slave_dev))
 		return NOTIFY_DONE;
@@ -257,12 +247,46 @@ static int nss_lag_update_slave(struct netdev_notifier_info *info)
 	if (slave_ifnum < 0)
 		return NOTIFY_DONE;
 
-	if (cu_info->linking) {
-		nss_lag_info("Interface %s added in LAG: %s\n" ,
-				slave_dev->name, cu_info->upper_dev->name);
+	bond_dev = netdev_master_upper_dev_get(slave_dev);
+	if (bond_dev) {
+
+		/* bond_dev is valid when slave is Enslaved */
+
+		nss_lag_info("Interface %s added in LAG ID=%d: %s\n",
+				slave_dev->name, bondid, bond_dev->name);
+
+		/*
+		 * Figure out the aggregation id of this slave
+		 */
+		bondid = bond_get_id(bond_dev);
+		if (bondid == NSS_LAG_BOND_ID_INVALID) {
+			nss_lag_warn("Invalid LAG group id 0x%x\n", bondid);
+			return NOTIFY_DONE;
+		}
+
+		spin_lock(&nss_lag_spinlock);
+
+		pvt = &bond_pvt[bondid];
+		pvt->bondid = bondid;
+
+		for (i = 0; i < NSS_LAG_MAX_SLAVES_PER_BONDID; i++) {
+			if (!pvt->slaves[i])
+				break;
+		}
+
+		if (i == NSS_LAG_MAX_SLAVES_PER_BONDID) {
+			spin_unlock(&nss_lag_spinlock);
+			nss_lag_warn("More than max %s slaves are added\n",
+					NSS_LAG_MAX_SLAVES_PER_BONDID);
+			return NOTIFY_DONE;
+		}
+
+		pvt->slaves[i] = slave_dev;
+		spin_unlock(&nss_lag_spinlock);
+
 		if (nss_lag_send_lag_state(nss_lag_nss_context,
-			cu_info->upper_dev, slave_dev, NSS_LAG_ENSLAVE)) {
-			nss_lag_warn("Ensalve %s dev failed\n",
+			bondid, slave_dev, NSS_LAG_ENSLAVE)) {
+			nss_lag_warn("Enslave %s dev failed\n",
 					slave_dev->name);
 			return NOTIFY_BAD;
 		}
@@ -270,10 +294,31 @@ static int nss_lag_update_slave(struct netdev_notifier_info *info)
 		return NOTIFY_DONE;
 	}
 
-	nss_lag_info("Interface %s removed from LAG: %s\n",
-			slave_dev->name, cu_info->upper_dev->name);
+	/* Slave is removed from bond */
+	spin_lock(&nss_lag_spinlock);
+	for (i = 0; i < NSS_LAG_MAX_BOND_DEVICES; i++) {
+		int j;
+
+		pvt = &bond_pvt[i];
+		for (j = 0; j < NSS_LAG_MAX_SLAVES_PER_BONDID; j++)
+			if (pvt->slaves[j] == slave_dev) {
+				pvt->slaves[j] = NULL;
+				bondid = pvt->bondid;
+			}
+
+		if (bondid != -1)
+			break;
+	}
+	spin_unlock(&nss_lag_spinlock);
+
+	if (bondid == -1)
+		return NOTIFY_DONE;
+
+	nss_lag_info("Interface %s removed from LAG ID=%d\n",
+			slave_dev->name, bondid);
+
 	if (nss_lag_send_lag_state(nss_lag_nss_context,
-		cu_info->upper_dev, slave_dev, NSS_LAG_RELEASE)) {
+		bondid, slave_dev, NSS_LAG_RELEASE)) {
 		nss_lag_warn("Deslave %s dev failed\n", slave_dev->name);
 		return NOTIFY_BAD;
 	}
@@ -288,11 +333,9 @@ static int nss_lag_update_slave(struct netdev_notifier_info *info)
 static int nss_lag_netdevice_event(struct notifier_block *unused,
 		unsigned long event, void *ptr)
 {
-	struct netdev_notifier_info *info = (struct netdev_notifier_info *) ptr;
-
 	switch (event) {
 	case NETDEV_CHANGEUPPER:
-		return nss_lag_update_slave(info);
+		return nss_lag_update_slave((struct netdev_notifier_info *)ptr);
 	}
 	return NOTIFY_DONE;
 }
